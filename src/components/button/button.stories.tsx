e673eba9ab88e21e903347a2a657e03ef8b49596
Simone Taeggi <startae14@gmail.com>
2024-05-06 23:35:55 +0200

    Adding Variants and Styling the Button

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -14,10 +14,14 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+  },
+};
 
 export const Secondary: Story = {
   args: {
-    children: 'Secondary',
+    variant: 'secondary',
   },
 };
